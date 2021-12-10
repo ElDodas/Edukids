4aba4b7638837b11cff1b6c77d61bb7d49ea956a
Rodrigo <rolibranl@hotmail.es>
2021-12-10 10:52:01 +0100

    v.2

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import functions as func # Se importa el archivo functions.py donde se encuentran todas las funciones que usa el programa
 
-logged = False
+logged, rachas = False, True
 User_dates = []
 
 func.listado_usuarios()
 
 while func.condicion1: #menú principal
+    if logged == True and rachas == True:
+        User_dates = func.rachas(User_dates)
+        rachas = False
+
     categoria0 = func.main_menu(logged,User_dates)
     if categoria0 == 1: # Llama a la función principal para elegir entre (1) juegos o (2) salir 
         func.condicion2 = True # Se vuelven a iniciar las condiciones de los bucles de los menús para poder recorrerlos todas la veces que queramos
@@ -14,45 +18,45 @@ while func.condicion1: #menú principal
             if categoria == 1: #matematicas
                 func.condicion3 = True # Se vuelven a iniciar la condicion del bucle para poder recorrerlos todas la veces que queramos
                 while func.condicion3: 
-                    categoria2 = func.matematicas(logged,User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
+                    categoria2 = func.matematicas(User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
                     if categoria2 == 1:
-                        func.buscanumeros()
+                        User_dates = func.buscanumeros(User_dates)
                     elif categoria2 == 2:
-                        func.sucesiones()
+                        User_dates = func.sucesiones(User_dates)
                     elif categoria2 == 3:
-                        func.operaciones()
+                        User_dates = func.operaciones(User_dates)
                     elif categoria2 == 4:
-                        func.problemas()
+                        User_dates = func.problemas(User_dates)
                     elif categoria2 == 5:
                         func.condicion3 = False
             elif categoria == 2: #lengua
                 func.condicion3 = True # Se vuelven a iniciar la condicion del bucle para poder recorrerlos todas la veces que queramos
                 while func.condicion3:
-                    categoria2 = func.lengua(logged,User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
+                    categoria2 = func.lengua(User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
                     if categoria2 == 1:
-                        func.ahorcado()
+                        User_dates = func.ahorcado(User_dates)
                     elif categoria2 == 2:
-                        func.sinonimos()   
+                        User_dates = func.sinonimos(User_dates)   
                     elif categoria2 == 3:
                         func.condicion3 = False
             elif categoria == 3: #ciencia
                 func.condicion3 = True # Se vuelven a iniciar la condicion del bucle para poder recorrerlos todas la veces que queramos
                 while func.condicion3:
-                    categoria2 = func.ciencia(logged,User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
+                    categoria2 = func.ciencia(User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
                     if categoria2 == 1:
-                        func.science_questions()
+                        User_dates = func.science_questions(User_dates)
                     elif categoria2 == 2:
-                        func.historic_dates()
+                        User_dates = func.historic_dates(User_dates)
                     elif categoria2 == 3:
-                        func.countries_questions()
+                        User_dates = func.countries_questions(User_dates)
                     elif categoria2 == 4:
                         func.condicion3 = False
             elif categoria == 4: #casual
                 func.condicion3 = True # Se vuelven a iniciar la condicion del bucle para poder recorrerlos todas la veces que queramos
                 while func.condicion3:
-                    categoria2 = func.casual(logged,User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
+                    categoria2 = func.casual(User_dates) # Según el número que nos devuelva la función se iniciará un juego o otro, o saldrá al menú anterior
                     if categoria2 == 1:
-                        func.trivial()
+                        User_dates = func.trivial(User_dates)
                     elif categoria2 == 2:
                         func.condicion3 = False
             elif categoria == 5: #Volver
@@ -64,10 +68,10 @@ while func.condicion1: #menú principal
             if categoria_loggin == 1:
                 logged_User = func.login() # Login devuelve una lista con el valor de si está loggeado y los datos del usuario
                 logged = logged_User[0]
-                User_dates = logged_User[1]
+                User_dates = [logged_User[1][0],logged_User[1][1]]
             elif categoria_loggin == 2:
                 func.register()
         else:
-            pass # Función estadísticas
+            func.estadisticas(User_dates) # Función estadísticas
     else:
         func.condicion1 = False
